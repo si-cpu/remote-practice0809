08212c8ffb809e00871127b85e0826848bb091ce
Si <tlgyrjs@naver.com>
2024-08-14 09:50:40 +0900

    DAY02 - JAVA 4일차 1교시

--- java-basic202408/src/prectice/practice1.java
+++ java-basic202408/src/prectice/practice1.java
@@ -2,15 +2,59 @@ package prectice;
 
 import basic.loop.WhileExample1;
 
-public class practice1
-{        /*
+import java.util.Scanner;
+
+public class practice1 {
+    public static void main(String[] args) {
+ /*
          1. 2가지의 정수를 1~100사이의 난수를 발생시켜서
           지속적으로 문제를 출제한 후 정답을 입력받으세요.
           사용자가 0을 입력하면 반복문을 탈출시키세요.
          2. 종료 이후에 정답 횟수와 오답 횟수를 각각 출력하세요.
          */
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("***연산 퀴즈***");
+        System.out.println("#종료하시려면 0을 입력해주세요.");
 
+        int cCount = 0;
+        int iCount = 0;
+
+        while(true){
+
+            int rn1 = (int) ((Math.random()*100)+ 1);
+            int rn2 = (int) ((Math.random()*100)+ 1);
+            int operator = (int)(Math.random()*2); // 0 or 1
+
+            int correct;
+            if (operator == 0){
+                System.out.printf("%d + %d = ???\n", rn1, rn2);
+                correct = rn1 + rn2;
+            }else {
+                System.out.printf("%d - %d = ???\n", rn1, rn2);
+                correct = rn1 +rn2;
+            }
+
+            System.out.println(" > ");
+            int answer = sc.nextInt();
+
+            if(answer == correct){
+                System.out.println("정답입니다!");
+                cCount++;
+            } else if (answer == 0) {
+                System.out.println("종료합니다!");
+                break;
+            }else {
+                System.out.println("틀렸습니다!");
+                iCount++;
+            }
+
+        }
+        System.out.println("=======================================");
+        System.out.println("정답횟수는  " + cCount + "회");
+        System.out.println("오답횟수는  " + iCount + "회");
+
+        sc.close();
 
 
 
@@ -48,4 +92,5 @@ public class practice1
         입력: 7, 3 -> "3부터 7까지의 누적합계: 25")
         - while문을 if else 로 나눠서 두번 쓰는 것은 지양하겠습니다.
        */
+    }
 }
---
+++ java-basic202408/src/prectice/practice2.java
@@ -0,0 +1,44 @@
+package prectice;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class practice2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("#먹고싶은 음식을 입력하세요.");
+        System.out.println("# 입력을 중지하려면 <stop> 이라고 입력하세요");
+
+        //음식 이름을 저장할 배열 -> 초기에는 입력값이 없으니 빈배열로 생성
+        String[] foodList = {};
+
+        while (true){
+            System.out.println("> ");
+            String newfood =sc.next();
+
+            if (newfood.equals("stop")) break;
+
+            //기존배열보다 큰 새 배열 생성
+            String[] temp = new String[foodList.length + 1];
+
+            // 기존 foodList에 있는 음식들 새 배열로 복사
+            for (int i = 0; i < foodList.length; i++){
+                temp[i] = foodList[i];
+            }
+
+            // 새 음식을 끝 인덱스에 추가하자
+            temp[temp.length - 1] = newfood;
+
+            // 주소값 이전
+            foodList = temp; temp = null;
+
+
+        }// end while
+
+        System.out.println("먹고 싶은 음식 리스트 = " + Arrays.toString(foodList));
+
+    }
+}
