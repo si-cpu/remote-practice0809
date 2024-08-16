e185d7fb72a97c1fb0bce598b7cbc7aeccd23f8f
Si <tlgyrjs@naver.com>
2024-08-16 09:32:44 +0900

    DAY02 - JAVA 5일차 1교시

--- java-basic202408/src/basic/method/MethodQuiz01.java
+++ java-basic202408/src/basic/method/MethodQuiz01.java
@@ -1,73 +1,70 @@
-/*
+
 package basic.method;
 
 import java.util.Arrays;
 
 public class MethodQuiz01 {
 
-	static String[] foods = {"chicken", "pasta", "beef", "fork"};
+    static String[] foods = {"chicken", "pasta", "beef", "fork"};
 
-	// 배열 내부 한눈에 보기
-	static void printFoods() {
-		System.out.println(Arrays.toString(foods));
-	}
+    // 배열 내부 한눈에 보기
+    static void printFoods() {
+        System.out.println(Arrays.toString(foods));
+    }
 
-	// 특정요소가 배열에 포함되어 있는지 확인
-	static boolean isInclude(String searchTarget) {
+    // 특정요소가 배열에 포함되어 있는지 확인
+    static boolean isInclude(String searchTarget) {
 //		int idx = indexOf(searchTarget);
 //		if(idx == -1) return false;
 //		else return true;
-		return indexOf(searchTarget) != -1;
-	}
+        return indexOf(searchTarget) != -1;
+    }
 
-	// foods 배열에서 특정 데이터의 인덱스를 반환
-	static int indexOf(String searchFood){
-		for (int i = 0; i < foods.length; i++) {
-			if (searchFood.equals(foods[i])){
-				return i;
-			}
-		}
-		return -1;
-	}
+    // foods 배열에서 특정 데이터의 인덱스를 반환
+    static int indexOf(String searchFood) {
+        for (int i = 0; i < foods.length; i++) {
+            if (searchFood.equals(foods[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	// foods 배열에 데이터를 끝에 추가하는 함수
-	static void push(String newFood){
-		String[] temp = makeNewArray();
-		String[] temp = copy(size : 1);
-		temp[temp.length-1]=newFood;
-		foods = temp;
-	}
+    // foods 배열에 데이터를 끝에 추가하는 함수
+    static void push(String newFood) {
+        String[] temp = makeNewArray();
+        String[] temp = copy(size :1);
+        temp[temp.length - 1] = newFood;
+        foods = temp;
+    }
 
-	// 푸쉬 인터뷰
-	 static String[] copy(int size) {
-		String[] temp = makeNewArray(size);
-		int loopCount = (size >= 0) ? foods.length : temp.length;
-		for (int i = 0; i < foods.length; i++) {
-			temp[i]=foods[i];
-		}// 추출: 드래그후  ctrl + alt + M
-		return temp;
+    // 푸쉬 인터뷰
+    static String[] copy(int size) {
+        String[] temp = makeNewArray(size);
+        int loopCount = (size >= 0) ? foods.length : temp.length;
+        for (int i = 0; i < foods.length; i++) {
+            temp[i] = foods[i];
+        }// 추출: 드래그후  ctrl + alt + M
+        return temp;
 
-		static void pop(){
-		String[] tmp = copy(size:1);
-		}
-	}
-
-
+        static void pop () {
+            String[] tmp = copy(size:1);
+        }
+    }
 
 
+    public static void main(String[] args) {
 
 
-	public static void main(String[] args) {
+        printFoods();
+        if (isInclude("bread")) {
+            System.out.println("존재함!");
+        } else {
+            System.out.println("존재하지 않음!");
+        }
 
 
-		printFoods();
-		if (isInclude("bread")){
-			System.out.println("존재함!");
-		}else{
-			System.out.println("존재하지 않음!");
-		}
-
-
+    }
 }
 
 
@@ -80,4 +77,4 @@ public class MethodQuiz01 {
 
 
 
-*/
+
