aa4142962c7f3259050b1939bdc65cf6f2a4bb6a
Si <tlgyrjs@naver.com>
2024-08-16 17:51:53 +0900

    DAY02 - JAVA 5일차 3교시

---
+++ Java_basic202408/src/oop/modi/protec/pac2/C.java
@@ -0,0 +1,26 @@
+package oop.modi.protec.pac2;
+
+import oop.modi.protec.pac1.A;
+
+public class C extends A {
+
+    C(){
+        super(100);// pretected
+    }
+
+
+    void test(){
+
+//        A a = new A(10); 상속받기 전 (x)
+//        A a = new A(3.14); 상속받기 전 (x)
+
+        super.f1 = 10;
+//        super.f2 = 20; 상속 받은 후(x) -> default
+
+
+        super.m1();
+//        super.m2();  상속 받은 후(x) -> default
+
+    }
+
+}
